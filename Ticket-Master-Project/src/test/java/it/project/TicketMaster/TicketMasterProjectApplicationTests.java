ccedd3b2122a22a2f6790990686897ec45788853
Roberto Inverni <91560500+R0b1n404@users.noreply.github.com>
2022-03-05 16:27:14 +0100

    Upload generic folder

---
+++ Ticket-Master-Project/src/test/java/it/project/TicketMaster/TicketMasterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package it.project.TicketMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketMasterProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
