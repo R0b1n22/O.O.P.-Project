d3dd7dacc7621362643d57fb1b44b03f5e25bb27
Roberto Inverni <91560500+R0b1n404@users.noreply.github.com>
2022-03-07 19:00:03 +0100

    Upload WrongPeriodException.java

--- Ticket-Master-Project/src/main/java/it/project/TicketMaster/exceptions/WrongPeriodException.java
+++ Ticket-Master-Project/src/main/java/it/project/TicketMaster/exceptions/WrongPeriodException.java
@@ -1,5 +1,26 @@
 package it.project.TicketMaster.exceptions;
 
-public class WrongPeriodException {
+public class WrongPeriodException extends Exception{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	String mex;
+	/**
+	 * Questo è il costruttore.
+	 * @param mex rappresenta il messaggio di errore.
+	 */
+	public WrongPeriodException(String mex) {
+		super();
+		this.mex = mex;
+	}
+	
+	/**
+	 * Restituisce un messaggio di errore passato al costruttore quando viene inserito un numero non ammesso.
+	 * @return String che contiene il messaggio d'errore che viene stampato.
+	 */
+	public String getMex() {
+		return mex;
+	}
 }
