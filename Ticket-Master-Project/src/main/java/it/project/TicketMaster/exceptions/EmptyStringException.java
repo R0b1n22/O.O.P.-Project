ab9a9131b55c80a023476edea95366865f79cf9e
Roberto Inverni <91560500+R0b1n404@users.noreply.github.com>
2022-03-07 18:57:37 +0100

    Upload EmptyStringException.java

--- Ticket-Master-Project/src/main/java/it/project/TicketMaster/exceptions/EmptyStringException.java
+++ Ticket-Master-Project/src/main/java/it/project/TicketMaster/exceptions/EmptyStringException.java
@@ -1,5 +1,27 @@
 package it.project.TicketMaster.exceptions;
 
-public class EmptyStringException {
+public class EmptyStringException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	String mex;
+	
+	/**
+	 * Questo è il costruttore.
+	 * @param mex rappresenta il messaggio di errore.
+	 */
+	public EmptyStringException(String mex) {
+		super();
+		this.mex=mex;
+	}
+	
+	/**
+	 * Restituisce un messaggio di errore passato al costruttore quando viene inserita una stringa vuota.
+	 * @return String che contiene il messaggio d'errore che viene stampato.
+	 */
+	public String getMex() {
+		return mex;
+	}
 }
