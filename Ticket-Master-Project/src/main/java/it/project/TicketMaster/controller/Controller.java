c0f8defbf00edbeb3a8e5564d1c35473ddced093
Roberto Inverni <91560500+R0b1n404@users.noreply.github.com>
2022-03-07 19:44:52 +0100

    Upload Controller.java

--- Ticket-Master-Project/src/main/java/it/project/TicketMaster/controller/Controller.java
+++ Ticket-Master-Project/src/main/java/it/project/TicketMaster/controller/Controller.java
@@ -25,20 +25,31 @@ public class Controller {
 	ApiReader file = new ApiReader("https://app.ticketmaster.com/discovery/v2/events?apikey=" + api_key + "&locale=*&countryCode=CA");
 
 	
-	 @GetMapping(value="/events") 
-	 public ResponseEntity<Vector<Events>>getEvents() throws FileNotFoundException, IOException, ParseException{ 
-		 file.jsonParser(); 
-		 return new ResponseEntity<>(file.getter(), HttpStatus.OK); 
-	 }
+	@RequestMapping(value = "/events", method = RequestMethod.GET)
+    public ResponseEntity<Vector<Events>>getEvents() throws FileNotFoundException, IOException, ParseException,EmptyStringException{
+        ResponseEntity<Vector<Events>> response;
+        try {
+        	file.jsonParser();
+            response = new ResponseEntity<>(file.getter(), HttpStatus.OK);        
+        } catch(Exception e) {
+        	response = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return response;
+    }
 	 
 
-
-	@GetMapping(value="/cityEvents")
-	public ResponseEntity<Vector<Events>> getCityEvents(@RequestParam String city) throws IOException, ParseException{
-		ApiReader file1 = new ApiReader("https://app.ticketmaster.com/discovery/v2/events?apikey=" + api_key + "&locale=*&city=" + city + "&countryCode=CA");
-		file1.jsonParser();
-		return new ResponseEntity<> (file.getter(), HttpStatus.OK);
-	}
+	@RequestMapping(value = "/cityEvents", method = RequestMethod.GET)
+    public ResponseEntity<Vector<Events>>getCityEvents(@RequestParam("city") String city) throws CityNotFoundException, EmptyStringException, WrongParamException, IOException, ParseException{
+        ResponseEntity<Vector<Events>> response;
+        try {
+        	ApiReader file1 = new ApiReader("https://app.ticketmaster.com/discovery/v2/events?apikey=" + api_key + "&locale=*&city=" + city + "&countryCode=CA");
+        	file1.jsonParser();
+        	response = new ResponseEntity<>(file.getter(), HttpStatus.OK);        
+        } catch(Exception e) {
+        	response = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return response;
+    }
 
 	//@GetMapping(value="/stats")
 
