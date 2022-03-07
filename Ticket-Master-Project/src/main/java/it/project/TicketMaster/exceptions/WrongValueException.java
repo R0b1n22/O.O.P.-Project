3b68b3750f2ee10a7543318b2051bf823d3b43b7
Roberto Inverni <91560500+R0b1n404@users.noreply.github.com>
2022-03-07 19:01:00 +0100

    Upload WrongValueException.java

--- Ticket-Master-Project/src/main/java/it/project/TicketMaster/exceptions/WrongValueException.java
+++ Ticket-Master-Project/src/main/java/it/project/TicketMaster/exceptions/WrongValueException.java
@@ -1,5 +1,26 @@
 package it.project.TicketMaster.exceptions;
 
-public class WrongValueException {
+public class WrongValueException extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	String mex;
+	/**
+	 * Questo è il costruttore.
+	 * @param mex rappresenta il messaggio di errore.
+	 */
+	public WrongValueException(String mex) {
+		super();
+		this.mex = mex;
+	}
+	
+	/**
+	 * Restituisce un messaggio di errore passato al costruttore quando viene inserita una stringa non ammessa per il value.
+	 * @return String che contiene il messaggio d'errore che viene stampato.
+	 */
+	public String getMex() {
+		return mex;
+	}
 
 }
