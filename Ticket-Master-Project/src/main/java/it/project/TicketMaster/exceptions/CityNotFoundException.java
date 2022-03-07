84cc1d3ab8a9c79070da498023774b672b368cd3
Roberto Inverni <91560500+R0b1n404@users.noreply.github.com>
2022-03-07 18:56:29 +0100

    Upload CityNotFoundException.java

--- Ticket-Master-Project/src/main/java/it/project/TicketMaster/exceptions/CityNotFoundException.java
+++ Ticket-Master-Project/src/main/java/it/project/TicketMaster/exceptions/CityNotFoundException.java
@@ -1,5 +1,29 @@
 package it.project.TicketMaster.exceptions;
 
-public class CityNotFoundException {
+public class CityNotFoundException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	String mex;
+	
+	/**
+	 * Questo è il costruttore.
+	 * @param mex rappresenta il messaggio di errore.
+	 */
+	
+	public CityNotFoundException(String mex) {
+		super();
+		this.mex = mex;
+	}
+	
+	/**
+	 * Restituisce un messaggio di errore passato dal costruttore quando il nome della città non è stato trovato.
+	 * @return String che contiene il messaggio d'errore che viene stampato.
+	 */
+	public String getMex() {
+		return mex;
+	}
 
 }
