45a461625df9b950b20ed274ce73eca589f47006
Daniel Tridici <93675113+Dan13L01@users.noreply.github.com>
2022-03-09 11:38:21 +0100

    Update ApiReader.java
    Adding constructor and attributes.

--- Ticket-Master-Project/src/main/java/it/project/TicketMaster/model/ApiReader.java
+++ Ticket-Master-Project/src/main/java/it/project/TicketMaster/model/ApiReader.java
@@ -1,5 +1,12 @@
 package it.project.TicketMaster.model;
 
 public class ApiReader {
-
+ 
+  private String url;
+	private JSONObject jsonR;
+	private Vector<Event> eventi = new Vector<Event>();
+	
+	public ApiReader (String url) throws FileNotFoundException, IOException, ParseException {
+		this.url = url;
+	}
 }
