d944e81d553e9fb2f313768854885bb4e603f78d
Daniel Tridici <93675113+Dan13L01@users.noreply.github.com>
2022-03-09 11:33:42 +0100

    Update Event.java
    Adding attributes, getters and setters.

--- Ticket-Master-Project/src/main/java/it/project/TicketMaster/model/Event.java
+++ Ticket-Master-Project/src/main/java/it/project/TicketMaster/model/Event.java
@@ -1,5 +1,86 @@
 package it.project.TicketMaster.model;
 
 public class Event {
-
+  private String id;
+	private String name;
+	private String genre;
+	private String subgenre;
+	private String promoter;
+	private String date;
+	private String hour;
+	private String country;
+	private String state;
+	private String city;
+	private String venue;
+	
+	public Event (String id) {
+		this.id = id;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	public String getSubgenre() {
+		return subgenre;
+	}
+	public void setSubgenre(String subgenre) {
+		this.subgenre = subgenre;
+	}
+	public String getPromoter() {
+		return promoter;
+	}
+	public void setPromoter(String promoter) {
+		this.promoter = promoter;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getHour() {
+		return hour;
+	}
+	public void setHour(String hour) {
+		this.hour = hour;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getVenue() {
+		return venue;
+	}
+	public void setVenue(String venue) {
+		this.venue = venue;
+	}
 }
