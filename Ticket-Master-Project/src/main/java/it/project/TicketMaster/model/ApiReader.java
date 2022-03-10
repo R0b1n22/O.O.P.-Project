9ca1c743291aa1ac0e472550e5d7b7c8f6ae790c
Daniel Tridici <93675113+Dan13L01@users.noreply.github.com>
2022-03-10 16:07:48 +0100

    Update ApiReader.java
    Addition of getter, setter and parser for jsonR

--- Ticket-Master-Project/src/main/java/it/project/TicketMaster/model/ApiReader.java
+++ Ticket-Master-Project/src/main/java/it/project/TicketMaster/model/ApiReader.java
@@ -1,12 +1,28 @@
 package it.project.TicketMaster.model;
 
 public class ApiReader {
- 
-  private String url;
+//ATTRIBUTES
+	private String url;
 	private JSONObject jsonR;
 	private Vector<Event> eventi = new Vector<Event>();
-	
+//BUILDER	
 	public ApiReader (String url) throws FileNotFoundException, IOException, ParseException {
 		this.url = url;
+	}
+//GETTER & SETTER	
+	public JSONObject getJsonobj() {
+		return jsonR;
+	}
+
+	public void setJsonobj(JSONObject jsonobj) {
+		this.jsonR = jsonobj;
+	}
+//PARSER	
+	public void jsonParser () throws FileNotFoundException, IOException, ParseException {
+		URL url = new URL(this.url);
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		BufferedReader read = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		JSONParser parser = new JSONParser();
+		this.jsonR = (JSONObject) parser.parse(read);
 	}
 }
