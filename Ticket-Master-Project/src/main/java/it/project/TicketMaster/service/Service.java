a96676719c16b795c344b06c92be796c792b5497
Roberto Inverni <91560500+R0b1n404@users.noreply.github.com>
2022-03-11 12:17:58 +0100

    Update Service.java
    Creation of service interface

--- Ticket-Master-Project/src/main/java/it/project/TicketMaster/service/Service.java
+++ Ticket-Master-Project/src/main/java/it/project/TicketMaster/service/Service.java
@@ -1,5 +1,17 @@
 package it.project.TicketMaster.service;
 
-public class Service {
+import java.util.Vector;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.springframework.stereotype.Component;
+
+import it.project.TicketMaster.model.Event;
+
+@Component
+public interface Service {
+
+	public abstract void calculator(Vector<Event>events);
+	public abstract void mediaCalculator(Vector<Long> num);	
+	public abstract JSONObject getStats();
 }
