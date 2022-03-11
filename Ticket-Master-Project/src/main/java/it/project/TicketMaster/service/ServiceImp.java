588a3999a6292712f89a4069466331d383736a0e
Roberto Inverni <91560500+R0b1n404@users.noreply.github.com>
2022-03-11 11:46:03 +0100

    Update ServiceImp.java
    Creations of the first methods useful to the controller

--- Ticket-Master-Project/src/main/java/it/project/TicketMaster/service/ServiceImp.java
+++ Ticket-Master-Project/src/main/java/it/project/TicketMaster/service/ServiceImp.java
@@ -1,5 +1,49 @@
 package it.project.TicketMaster.service;
 
-public class ServiceImp {
+import org.springframework.stereotype.Service;
+//import org.springframework.web.client.RestTemplate;
+import it.project.TicketMaster.model.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Vector;
+
+import org.json.simple.*;
+
+@Service
+public abstract class ServiceImp implements it.project.TicketMaster.service.Service{
+
+	private static JSONObject out = new JSONObject();
+	//private Vector<Event>events;
+	
+	//BUILDER()
+	public ServiceImp() 
+	{
+		super();
+	}
+	
+	@Override
+	public void calculator(Vector<Event>events)
+	{
+		out.put("Events", events.size());
+	}
+
+	@Override
+	public void mediaCalculator(Vector<Long> num)
+	{
+		Long sum = null;
+		Double Media = null;
+		for(int i = 0 ; i < 12; i++)
+		{
+			sum += num.get(i);
+		}
+		Media = (double) (sum/12);
+		out.put("Monthly average: ", Media);
+	}
+		
+	@Override
+	public JSONObject getStats()
+	{
+		return out;
+	}
 }
