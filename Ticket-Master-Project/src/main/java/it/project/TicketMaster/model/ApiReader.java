737270a36aab726b75ddb203c0c6291b1019ea32
Daniel Tridici <93675113+Dan13L01@users.noreply.github.com>
2022-03-13 14:36:47 +0100

    Update ApiReader.java
    Update of the getter method with the addition of the reader.

--- Ticket-Master-Project/src/main/java/it/project/TicketMaster/model/ApiReader.java
+++ Ticket-Master-Project/src/main/java/it/project/TicketMaster/model/ApiReader.java
@@ -67,28 +67,33 @@ public class ApiReader {
 		JSONObject event;
 		JSONObject embedded = (JSONObject) jsonR.get("_embedded");
 		JSONArray jEvents = (JSONArray) embedded.get("events");
-		for (int i = 0; i < jEvents.size(); i++) {
-		    event = ((JSONObject) jEvents.get(i));
-		    Event e = new Event ((String) event.get("id"));
-		    e.setName((String) event.get("name"));
-		    e.setUrl((String) event.get("url"));
-		    e.setDate((String) ((JSONObject) ((JSONObject) event.get("dates")).get("start")).get("localDate"));
-		    e.setHour((String) ((JSONObject) ((JSONObject) event.get("dates")).get("start")).get("localTime"));
-		    e.setGenre((String) ((JSONObject) ((JSONObject) ((JSONArray) event.get("classifications")).get(0)).get("genre")).get("name"));
-		    e.setSubgenre((String) ((JSONObject) ((JSONObject) ((JSONArray) event.get("classifications")).get(0)).get("subGenre")).get("name"));
-		    e.setPromoter((String) ((JSONObject) event.getOrDefault("promoter", error)).get("name"));
-		    JSONObject obj = (JSONObject) ((JSONArray) event.getOrDefault("priceRanges", arr)).get(0);
-		    obj.remove("currency");
-		    obj.remove("type");
-		    e.setPriceRanges(obj); 
-		    e.setTicketsAvailable((Long) ((JSONObject) event.getOrDefault("accessibility", error)).get("ticketLimit"));
-		    e.setAgeRestriction((JSONObject) event.getOrDefault("ageRestrictions", error));
-		    e.setHealthCheck((JSONObject) ((JSONObject) event.getOrDefault("ticketing", error)).getOrDefault("healthCheck", error));
-		    e.setVenue((String) ((JSONObject) ((JSONArray) ((JSONObject) event.get("_embedded")).get("venues")).get(0)).get("name"));
-		    e.setCity((String) ((JSONObject) ((JSONObject) ((JSONArray) ((JSONObject) event.get("_embedded")).get("venues")).get(0)).get("city")).get("name"));
-		    e.setState((String) ((JSONObject) ((JSONObject) ((JSONArray) ((JSONObject) event.get("_embedded")).get("venues")).get(0)).get("state")).get("name"));
-		    e.setCountry((String) ((JSONObject) ((JSONObject) ((JSONArray) ((JSONObject) event.get("_embedded")).get("venues")).get(0)).get("country")).get("name"));
-		    events.add(e);  
+		Long num = (Long) ((JSONObject) jsonR.get("page")).get("totalPages");
+		for (long j = 0; j < num; j++) {
+			if(this.reader(j)) {
+				for (int i = 0; i < jEvents.size(); i++) {
+				    event = ((JSONObject) jEvents.get(i));
+				    Event e = new Event ((String) event.get("id"));
+				    e.setName((String) event.get("name"));
+				    e.setUrl((String) event.get("url"));
+				    e.setDate((String) ((JSONObject) ((JSONObject) event.get("dates")).get("start")).get("localDate"));
+				    e.setHour((String) ((JSONObject) ((JSONObject) event.get("dates")).get("start")).get("localTime"));
+				    e.setGenre((String) ((JSONObject) ((JSONObject) ((JSONArray) event.get("classifications")).get(0)).get("genre")).get("name"));
+				    e.setSubgenre((String) ((JSONObject) ((JSONObject) ((JSONArray) event.get("classifications")).get(0)).get("subGenre")).get("name"));
+				    e.setPromoter((String) ((JSONObject) event.getOrDefault("promoter", error)).get("name"));
+				    JSONObject obj = (JSONObject) ((JSONArray) event.getOrDefault("priceRanges", arr)).get(0);
+				    obj.remove("currency");
+				    obj.remove("type");
+				    e.setPriceRanges(obj); 
+				    e.setTicketsAvailable((Long) ((JSONObject) event.getOrDefault("accessibility", error)).get("ticketLimit"));
+				    e.setAgeRestriction((JSONObject) event.getOrDefault("ageRestrictions", error));
+				    e.setHealthCheck((JSONObject) ((JSONObject) event.getOrDefault("ticketing", error)).getOrDefault("healthCheck", error));
+				    e.setVenue((String) ((JSONObject) ((JSONArray) ((JSONObject) event.get("_embedded")).get("venues")).get(0)).get("name"));
+				    e.setCity((String) ((JSONObject) ((JSONObject) ((JSONArray) ((JSONObject) event.get("_embedded")).get("venues")).get(0)).get("city")).get("name"));
+				    e.setState((String) ((JSONObject) ((JSONObject) ((JSONArray) ((JSONObject) event.get("_embedded")).get("venues")).get(0)).get("state")).get("name"));
+				    e.setCountry((String) ((JSONObject) ((JSONObject) ((JSONArray) ((JSONObject) event.get("_embedded")).get("venues")).get(0)).get("country")).get("name"));
+				    events.add(e);  
+				}
+			} else return events;
 		}
 		return events;
 	}
