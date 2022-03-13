d8320afac509f2875f2d2676d8935ad3a93b17c1
Daniel Tridici <93675113+Dan13L01@users.noreply.github.com>
2022-03-13 18:25:06 +0100

    Update Window.java
    Adding attributes and builder.

--- Ticket-Master-Project/src/main/java/it/project/TicketMaster/gui/Window.java
+++ Ticket-Master-Project/src/main/java/it/project/TicketMaster/gui/Window.java
@@ -1,5 +1,15 @@
 package it.project.TicketMaster.gui;
 
-public class Window {
-
+public class Window extends JFrame{
+  //ATTRIBUTI
+	JButton ev_Stats = new JButton("Mostra Statistiche");
+	JButton ev_State_Stats = new JButton("Mostra Statistiche");
+	JButton ev_City_Stats = new JButton("Mostra Statistiche");
+  //BUILDER
+  public Window (String titolo) { 	
+		super(titolo);
+		this.setSize(700, 200);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
+	}
 }
