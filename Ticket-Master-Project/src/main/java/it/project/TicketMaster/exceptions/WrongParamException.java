9c89e57b62ef505eedd4cf959247e559913a405b
Roberto Inverni <91560500+R0b1n404@users.noreply.github.com>
2022-03-15 22:32:41 +0100

    Update WrongParamException.java
    Fixed indentation

--- Ticket-Master-Project/src/main/java/it/project/TicketMaster/exceptions/WrongParamException.java
+++ Ticket-Master-Project/src/main/java/it/project/TicketMaster/exceptions/WrongParamException.java
@@ -2,24 +2,24 @@ package it.project.TicketMaster.exceptions;
 
 public class WrongParamException extends Exception{
 
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
 	String mex;
+	
 	/**
-	 * Questo è il costruttore.
 	 * @param mex rappresenta il messaggio di errore.
 	 */
+	
 	public WrongParamException(String mex) {
 		super();
 		this.mex = mex;
 	}
 	
 	/**
-	 * Restituisce un messaggio di errore passato al costruttore quando viene inserita una stringa non ammessa per param.
+	 * Restituisce un messaggio di errore passato al costruttore quando viene inserita una stringa non ammessa come parametro.
 	 * @return String che contiene il messaggio d'errore che viene stampato.
 	 */
+	
 	public String getMex() {
 		return mex;
 	}
