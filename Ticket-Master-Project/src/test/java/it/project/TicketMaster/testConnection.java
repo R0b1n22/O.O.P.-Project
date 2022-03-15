47d540a704feea0304fc2685819e4e368f01958a
Roberto Inverni <91560500+R0b1n404@users.noreply.github.com>
2022-03-15 11:49:40 +0100

    Update testConnection.java

--- Ticket-Master-Project/src/test/java/it/project/TicketMaster/testConnection.java
+++ Ticket-Master-Project/src/test/java/it/project/TicketMaster/testConnection.java
@@ -1,59 +1,71 @@
 package it.project.TicketMaster;
 
-import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-//import java.net.http.HttpClient;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-public class test {
+class testConnection {
 
-	 
-	@Before
-	public void setUp() throws Exception {
-		String api_key = "7elxdku9GGG5k8j0Xm8KWdANDgecHMV0";
-		URL url = new URL("https://app.ticketmaster.com/discovery/v2/events?apikey=" + api_key);
+	static String api_key = "7elxdku9GGG5k8j0Xm8KWdANDgecHMV0";
+	static URL url;
+	static HttpURLConnection connection;
+	static int responseCode;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
 		
 		try {
-			 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-			 InputStream inputStream = null;
-			 BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
-			 StringBuilder response = new StringBuilder();
-			 String currentLine;
-			 
-			 int responseCode = connection.getResponseCode();
-			    
-			    if (responseCode == 200) {
-			        inputStream = connection.getInputStream();
-			    } else {
-			        inputStream = connection.getErrorStream();
-			    }
-	
-			    while ((currentLine = in.readLine()) != null) 
-			        response.append(currentLine);
-	
-			    in.close();
-		    
-			}catch(Exception e) {System.err.println(e);}
-		 		
+			
+			url = new URL("https://app.ticketmaster.com/discovery/v2/events?apikey=" + api_key);
+		} catch (MalformedURLException e1) {
+			
+			e1.printStackTrace();
+		}
+		
+		response = new StringBuilder();
+		
+		try {
+			connection = (HttpURLConnection) url.openConnection();
+			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			String currentLine;
+			responseCode = connection.getResponseCode();
+		
+		}catch(Exception e) {System.err.println(e);}
+		
 	}
-	
-	@After
-	public void tearDown() throws Exception{
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
 		connection = null;
 		assertNull(connection);
+		in.close();
 	}
-	
+
 	@Test
-	void contextLoads() {
-     return response.toString();
+	void testApi() throws IOException {
+		if(responseCode == 200)
+			{
+				System.out.println("200");
+			}
+			else
+			{
+				System.err.println("Error");
+			}
+		
+			while((currentLine = in.readLine()) != null)
+			{
+				response.append(currentLine);				
+			}
 	}
+
 }
