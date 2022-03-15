107b6af18a6dc8e71cfb9d0e536da6431697184c
Roberto Inverni <91560500+R0b1n404@users.noreply.github.com>
2022-03-15 22:31:22 +0100

    Update EmptyStringException.java
    Fixed indentation

--- Ticket-Master-Project/src/main/java/it/project/TicketMaster/exceptions/EmptyStringException.java
+++ Ticket-Master-Project/src/main/java/it/project/TicketMaster/exceptions/EmptyStringException.java
@@ -2,16 +2,14 @@ package it.project.TicketMaster.exceptions;
 
 public class EmptyStringException extends Exception {
 
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 	String mex;
 	
 	/**
-	 * Questo è il costruttore.
 	 * @param mex rappresenta il messaggio di errore.
 	 */
+	
 	public EmptyStringException(String mex) {
 		super();
 		this.mex=mex;
@@ -21,7 +19,9 @@ public class EmptyStringException extends Exception {
 	 * Restituisce un messaggio di errore passato al costruttore quando viene inserita una stringa vuota.
 	 * @return String che contiene il messaggio d'errore che viene stampato.
 	 */
+	
 	public String getMex() {
 		return mex;
 	}
 }
+
