38569bc499a8055d00357d8561df3691ada53cde
Roberto Inverni <91560500+R0b1n404@users.noreply.github.com>
2022-03-15 12:05:26 +0100

    Update testConnection.java
    Almost completely changed the class

--- Ticket-Master-Project/src/test/java/it/project/TicketMaster/testConnection.java
+++ Ticket-Master-Project/src/test/java/it/project/TicketMaster/testConnection.java
@@ -2,10 +2,9 @@ package it.project.TicketMaster;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.BufferedReader;
+
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -19,27 +18,30 @@ class testConnection {
 	static String api_key = "7elxdku9GGG5k8j0Xm8KWdANDgecHMV0";
 	static URL url;
 	static HttpURLConnection connection;
-	static int responseCode;
+	static StringBuilder response;
+	static int code;
+	
 	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 		
 		try {
-			
 			url = new URL("https://app.ticketmaster.com/discovery/v2/events?apikey=" + api_key);
 		} catch (MalformedURLException e1) {
-			
 			e1.printStackTrace();
 		}
 		
 		response = new StringBuilder();
 		
 		try {
-			connection = (HttpURLConnection) url.openConnection();
-			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			String currentLine;
-			responseCode = connection.getResponseCode();
-		
+			
+	        connection = (HttpURLConnection)url.openConnection();
+	        connection.setRequestMethod("GET");
+	        connection.connect();
+
+	        code = connection.getResponseCode();
+	        System.out.println("Response code of the object is " + code);
+
 		}catch(Exception e) {System.err.println(e);}
 		
 	}
@@ -48,24 +50,15 @@ class testConnection {
 	static void tearDownAfterClass() throws Exception {
 		connection = null;
 		assertNull(connection);
-		in.close();
 	}
 
 	@Test
 	void testApi() throws IOException {
-		if(responseCode == 200)
-			{
-				System.out.println("200");
-			}
-			else
-			{
-				System.err.println("Error");
-			}
-		
-			while((currentLine = in.readLine()) != null)
-			{
-				response.append(currentLine);				
-			}
+		if (code==200)
+		{
+		    System.out.println("OK");
+		}
 	}
 
 }
+
