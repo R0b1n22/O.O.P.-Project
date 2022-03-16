3e90329013bacb6ff5cb90525647498914ed3d06
Daniel Tridici <93675113+Dan13L01@users.noreply.github.com>
2022-03-16 18:45:23 +0100

    Update TicketMasterProjectApplication.java
    Updating main.

--- Ticket-Master-Project/src/main/java/it/project/TicketMaster/TicketMasterProjectApplication.java
+++ Ticket-Master-Project/src/main/java/it/project/TicketMaster/TicketMasterProjectApplication.java
@@ -3,11 +3,23 @@ package it.project.TicketMaster;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import it.project.TicketMaster.exceptions.EmptyStringException;
+import it.project.TicketMaster.exceptions.WrongParamException;
+import it.project.TicketMaster.gui.Window;
+
 @SpringBootApplication
 public class TicketMasterProjectApplication {
-
-	public static void main(String[] args) {
+	/**
+	 * Main
+	 * @param args
+	 * @throws EmptyStringException
+	 * @throws WrongParamException
+	 */
+	public static void main(String[] args) throws EmptyStringException, WrongParamException {
+		Window w = new Window ("TicketAdvisor");
+		w.init();
+		w.Listener();
 		SpringApplication.run(TicketMasterProjectApplication.class, args);
+		w.setVisible(true);
 	}
-
 }
